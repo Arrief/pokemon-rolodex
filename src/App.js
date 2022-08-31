728dbe12225f25413553061098a0cf1e7bf4a8a7
Arne Rief <arne.rief@gmail.com>
2022-08-31 20:40:57 +0200

    API call with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor method available on all class cmp, allows us to access local state
+  constructor() {
+    // super calls constructor methods of any parent classes we are extending (here: React.Component)
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  // similar to useEffect hook, perform API request when component is rendered for 1st time
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=30")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(
+          // updater fn to update state with option to pass state & props as arguments (not needed here!), if we want to update state based on previous values
+          (state, props) => {
+            return { monsters: data };
+          },
+          // callback fn triggered after updater fn finished, to check if state was updated
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
