565fd23c4ed79e6001e6abc3c1214753da81b8e2
Arne Rief <arne.rief@gmail.com>
2025-12-26 23:43:27 +0100

    Fix search & detail view UI

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -11,17 +11,34 @@ class CardList extends Component {
     this.state = {
       currentPage: 1,
     };
+  }
+
+// If the length of the monsters array changed in the parent component, reset to page 1
+  componentDidUpdate(prevProps) {
+    if (prevProps.monsters.length !== this.props.monsters.length) {
+      this.setState({ currentPage: 1 });
+    }
+  }
 
     // functions to change pages, boolean check when called onClick will disable the respective button if it is the first/last page
-    this.prevPage = () => this.setState(() => --this.state.currentPage);
-    this.nextPage = () => this.setState(() => ++this.state.currentPage);
-  }
+  prevPage = () => {
+    this.setState((prevState) => ({
+      currentPage: prevState.currentPage - 1,
+    }));
+  };
+
+  nextPage = () => {
+    this.setState((prevState) => ({
+      currentPage: prevState.currentPage + 1,
+    }));
+  };
 
   render() {
     const { monsters } = this.props;
-    // pagination calculations
     const currentPage = this.state.currentPage;
-    const totalPages = Math.ceil(monsters.length / this.monstersPerPage);
+
+    // pagination calculations
+    const totalPages = Math.ceil(monsters.length / this.monstersPerPage) || 1;
     const lastDisplayIndex = currentPage * this.monstersPerPage;
     const firstDisplayIndex = lastDisplayIndex - this.monstersPerPage;
     const currentlyVisible = monsters.slice(
@@ -33,17 +50,17 @@ class CardList extends Component {
       <>
         <div className="card-list">
           {currentlyVisible.map((monster, id) => (
-            <Card monster={monster} id={id} />
+            <Card monster={monster} id={`${id}-${monster.name}`} key={`${id}-${monster.name}`} />
           ))}
         </div>
         <div className="pages">
           <p>
             Page {currentPage} / {totalPages}
           </p>
-          <button disabled={currentPage === 1} onClick={this.prevPage}>
+          <button disabled={currentPage <= 1} onClick={this.prevPage}>
             ← Back
           </button>
-          <button disabled={currentPage === totalPages} onClick={this.nextPage}>
+          <button disabled={currentPage >= totalPages} onClick={this.nextPage}>
             Next →
           </button>
         </div>
